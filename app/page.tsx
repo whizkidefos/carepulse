532ba0b331ec064abcf71c30a14b0c248e6ea2b4
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-09-27 17:55:03 +0100

    added assets and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl underline">CarePulse</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient logo"
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
   );
 }
